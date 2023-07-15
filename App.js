b4b63503e204e1149f5ac3d182db870f0bd94ad1
Nuttachai-K <korn.nuttachai.jp@gmail.com>
2023-07-15 12:08:30 +0700

    Date{15/07/2023}

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Cat from './components/cat';
+import ViewBoxsWithColorAndText from './components/ViewBoxsWithColorAndText'
+import DisplayAndImage from './components/DisplayAndImage';
+import LotsOfGreeting from './components/LotsOfGreeting';
+import CustomText from './components/CustomText';
 
 export default function App() {
   return (
-    <View>
-      <Cat/>
+    <View style={styles.container}>
+      {/* <ViewBoxsWithColorAndText/> */}
+      {/* <DisplayAndImage/> */}
+      {/* <LotsOfGreeting/> */}
+      <CustomText/>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container:{
+     flex:1,   
+     justifyContent:'center',    
+     alignItems:'center'   
+  }
+ })
+ 
---
+++ components/CustomText.js
@@ -0,0 +1,22 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MyCustomTextWith = ({fName,lName})=> {
+    return (
+        <View>
+            <Text>Your First Name is {fName} and Last name is {lName} </Text>
+        </View>
+    )
+}
+
+const CustomText = () => {
+  return (
+    <View>
+      <MyCustomTextWith fName="Nuttachai" lName="Phusadeekunpaisan"/>
+      <MyCustomTextWith fName="Paisan" lName="Kiatjanon"/>
+        
+    </View>
+  )
+}
+
+export default CustomText
