83a4548e97c7cbf3b97ad4f1bce18cbae8befd56
Nuttachai-K <korn.nuttachai.jp@gmail.com>
2023-07-22 11:46:23 +0700

    TextInputPractice Commit [Week4-July-22-2023]

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ViewBoxsWithColorAndText from './components/ViewBoxsWithColorAndText'
-import DisplayAndImage from './components/DisplayAndImage';
-import LotsOfGreeting from './components/LotsOfGreeting';
-import CustomText from './components/CustomText';
+import Counter from './components/Counter';
+import MyInput from './components/MyInput';
+import IncrementCounter from './components/IncrementCounter';
+import Form from './components/Form';
+import Login from './components/Login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <ViewBoxsWithColorAndText/> */}
-      {/* <DisplayAndImage/> */}
-      {/* <LotsOfGreeting/> */}
-      <CustomText/>
+      {/* <Counter/> */}
+      {/* <MyInput/> */}
+     {/* <IncrementCounter/> */}
+    {/* <Form/> */}
+    <Login/>
     </View>
   );
 }
---
+++ components/Login.js
@@ -0,0 +1,65 @@
+import { StyleSheet, Text, View, TextInput ,Button} from "react-native";
+import React, { useState } from "react";
+
+export default function Login() {
+  const checkTextInput = () => {
+    if (!textInputName.trim()) {
+      alert("Plese Enter Name");
+      return;
+    }
+    if (!textInputEmail.trim()){
+        alert("Plese Enter Email");
+        return;
+    }
+    if ((textInputEmail.split('@')).length != 2) {
+        alert("Plese Enter correct Email");
+        return;
+    }
+    alert("Success")
+  };
+
+  const [textInputName, setName] = useState("");
+  const [textInputEmail, setEmail] = useState("");
+
+  return (
+    <View style = {styles.container}>
+      <TextInput
+        placeholder="Enter Name"
+        style={styles.textInputStyle}
+        value={textInputName}
+        onChangeText={(value) => {
+          setName(value);
+        }}
+      />
+      <TextInput
+        placeholder="Enter Email"
+        style={styles.textInputStyle}
+        value={textInputEmail}
+        onChangeText={(value) => {
+          setEmail(value);
+        }}
+      />
+      <Text>{'\n'}</Text>
+       <Button  
+        title='Submit'
+        onPress={checkTextInput}
+
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 35,
+  },
+  textInputStyle: {
+    width: "100%",
+    height: 40,
+    paddingHorizontal: 5,
+    borderWidth: 0.5,
+    marginTop: 15,
+    color: 'grey'
+  },
+});
