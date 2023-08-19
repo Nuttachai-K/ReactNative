8bf716fc3aed0cbc1001060819fe6fc4b2d4eeb0
Nuttachai-K <korn.nuttachai.jp@gmail.com>
2023-08-19 11:51:23 +0700

    Second Commit [Lecture7-August-19-2023]

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import FlatList_Example1 from './components/FlatList_Example1';
+import FlatList_HeaderFooter from './components/FlatList_HeaderFooter';
 
-import RandomUserScreen from './components/RandomUserScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-    <RandomUserScreen/>
+    <View>
+     <FlatList_HeaderFooter/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container:{
-     flex:1,   
-     justifyContent:'center',    
-     alignItems:'center'   
-  }
- })
+const styles = StyleSheet.create({})
  
