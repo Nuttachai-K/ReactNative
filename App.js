ff24b6dfddde0266a641dbe2c75a37c8475b96af
Nuttachai-K <korn.nuttachai.jp@gmail.com>
2023-08-26 11:47:38 +0700

    Second Commit [Lecture8-August-26-2023]

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import FlatListAPI from './components/FlatListAPI';
 import News from './components/News';
+import ProductScreen from './components/ProductScreen';
 
 
 export default function App() {
   return (
     <View>
-      <News/>
+      <ProductScreen/>
     </View>
   );
 }
